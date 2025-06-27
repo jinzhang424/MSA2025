419593ad514ccd15a17388dd205fb2eed6d0e2ed
Jin <ajhz011@aucklanduni.ac.nz>
2025-06-27 14:40:03 +1200

    Finished setting up postgresql database on Render. Also create a new User Model - this is an uncomplete model and is temporary

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>().HasKey(u => u.Id); // Explicitly set Id as the primary key
+    }
+}
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa18b4e6d0d98636e718cf11c4b712ac71e43e97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+896b635a2b59d9202e65983833a4c648f45e8f28")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
