d0e980f69d43f7683a7d34d1e8dd16cf18447bde
Jin <ajhz011@aucklanduni.ac.nz>
2025-06-29 15:48:41 +1200

    Implemented a way to accept a user's application

--- backend/Controllers/WaitingListController.cs
+++ backend/Controllers/WaitingListController.cs
@@ -52,18 +52,60 @@ public class WaitingListController(ApplicationDbContext context) : ControllerBas
         return Ok("Successfully removed from project waiting list");
     }
 
-    [HttpPut("RejectUserApplication/{userId}/{projectId}")]
-    public async Task<IActionResult> RejectUserApplication(int userId, int projectId)
+    public async void RemoveUserFromWaitingList(int userId, int projectId)
     {
         var waitingListUser = await _context.ProjectWaitingListUser
-            .FirstOrDefaultAsync(pwu => pwu.UserId == userId && pwu.ProjectId == projectId);
-        if (waitingListUser == null)
+            .FirstOrDefaultAsync(pwu => pwu.UserId == userId && pwu.ProjectId == projectId)
+            ?? throw new Exception("User not found in this project's waiting list");
+
+        _context.ProjectWaitingListUser.Remove(waitingListUser);
+        await _context.SaveChangesAsync();
+    }
+
+    [HttpPut("AcceptUserApplication/{userId}/{projectId}")]
+    public async Task<IActionResult> AcceptUserApplication(int userId, int projectId)
+    {
+        // Check if the user has applied to the project
+        var projectWaitingListUser = await _context.ProjectWaitingListUser
+            .AnyAsync(pwu => pwu.UserId == userId && pwu.ProjectId == projectId);
+        if (!projectWaitingListUser)
+        {
+            return NotFound("User does not exist or was not part of the waiting list");
+        }
+
+        var projectMember = new ProjectMember
+        {
+            ProjectId = projectId,
+            UserId = userId
+        };
+
+        await _context.ProjectMmembers.AddAsync(projectMember);
+        _context.SaveChanges();
+
+        // User no longer needs to be in waiting list and so it they can be removed
+        try
+        {
+            RemoveUserFromWaitingList(userId, projectId);
+        }
+        catch
         {
             return NotFound("User not found in this project's waiting list");
         }
 
-        _context.ProjectWaitingListUser.Remove(waitingListUser);
-        await _context.SaveChangesAsync();
+        return Ok("Successfully accepted user into project");
+    }
+
+    [HttpDelete("RejectUserApplication/{userId}/{projectId}")]
+    public IActionResult RejectUserApplication(int userId, int projectId)
+    {
+        try
+        {
+            RemoveUserFromWaitingList(userId, projectId);
+        }
+        catch 
+        {
+            return NotFound("User not found in this project's waiting list");
+        }
         
         return Ok("Successfully rejected user");
     }
