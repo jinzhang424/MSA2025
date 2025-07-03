650689742571f758493754848643352f4624cedf
Jin <ajhz011@aucklanduni.ac.nz>
2025-07-03 15:41:07 +1200

    Fixed Navbar to properly center the main links

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router"
 
 export default function NavBar() {
     return (
-        <div className="absolute flex items-center justify-between w-full p-5 pl-7 pr-7 z-10">
+        <div className="absolute grid grid-cols-3 items-center justify-between w-full p-5 pl-7 pr-7 z-10">
             <header className="text-3xl font-bold text-white">CoCreate</header>
             <div className="flex space-x-8 font-semibold text-white justify-self-center">
                 <Link to="/">Discover</Link>
@@ -10,7 +10,7 @@ export default function NavBar() {
                 <Link to="/">About Us</Link>
             </div>
 
-            <div className="flex items-center space-x-4 font-semibold">
+            <div className="flex items-center space-x-4 font-semibold justify-end">
                 <Link to="/" className="text-white">Login</Link>
                 <button className="p-2 pl-5 pr-5 text-white bg-black rounded-md">Join Us</button>
             </div>
