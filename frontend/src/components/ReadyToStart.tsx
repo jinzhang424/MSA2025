6e0aa7395f50c6ec08b27468f1da0ed48855097d
Jin <ajhz011@aucklanduni.ac.nz>
2025-07-04 22:21:36 +1200

    Made ReadyToStart responsive

--- frontend/src/components/ReadyToStart.tsx
+++ frontend/src/components/ReadyToStart.tsx
@@ -5,12 +5,12 @@ function ReadyToStart() {
         <div className="bg-purple-950 bg-gradient-to-br from-orange-300 p-16 text-center text-white space-y-12">
             <div className="flex flex-col justify-center items-center space-y-4">
                 <h1 className="text-white text-4xl font-bold">Ready To Start Your Journey?</h1>
-                <h2 className="max-w-1/2 text-lg">
+                <h2 className="min-w-[300px] max-w-1/2 text-lg">
                     Join our community of creators, developers, and designers to find your next collaborative project.
                 </h2>
             </div>
 
-            <div className="flex space-x-8 justify-center grid-cols-2">
+            <div className="flex gap-8 justify-center grid-cols-2 flex-wrap items-center">
                 <BGFadeButton className="p-4 pl-5 pr-5 w-44">
                     Discover Projects
                 </BGFadeButton>
