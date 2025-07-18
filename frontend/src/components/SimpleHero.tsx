8d6d8270c48eb8261607977bbbc6f407ee25307c
Jin <ajhz011@aucklanduni.ac.nz>
2025-07-18 14:08:52 +1200

    Added a button to navigate to the dashboard in simple hero

--- frontend/src/components/SimpleHero.tsx
+++ frontend/src/components/SimpleHero.tsx
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
 import BackLink from "./BackLink";
+import type { RootState } from "../store/store";
+import BGFadeButton from "./buttons/BGFadeButton";
+import { useNavigate } from "react-router";
 
 interface SimpleHeroProps{
     heading: string,
@@ -6,11 +10,22 @@ interface SimpleHeroProps{
 }
 
 function SimpleHero({ heading, subheading } : SimpleHeroProps) {
+    const user = useSelector((state: RootState) => state.user);
+    const navigate = useNavigate()
+
     return (
         <div className='flex flex-col gap-3 justify-center items-center bg-purple-950 bg-gradient-to-br from-orange-300 h-[400px] p-16'>
             <BackLink to="/">
                 Back to home
             </BackLink>
+
+            {user.token && (
+                <div className="absolute top-6 right-10">
+                    <BGFadeButton onClick={() => navigate('/dashboard')} className="px-5 py-2" bgFade={true}>
+                        Dashboard
+                    </BGFadeButton>
+                </div>
+            )}
             
             <h1 className='text-white text-4xl font-bold text-center'>{heading}</h1>
             <h2 className='text-slate-200 font-semibold text-center'>{subheading}</h2>
