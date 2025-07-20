efdb9ef6c7cb5b1fe3694c03d1a592f3acb8ec27
Jin <ajhz011@aucklanduni.ac.nz>
2025-07-20 15:19:00 +1200

    Changed BGFadeButton's isLoading default value to false

--- frontend/src/components/buttons/BGFadeButton.tsx
+++ frontend/src/components/buttons/BGFadeButton.tsx
@@ -17,7 +17,7 @@ interface ButtonProps {
  * @param onClick onClick handler
  * @returns A button that if bgFade is true, background will fade from 0 to full opacity
  */
-function BGFadeButton({ bgFade = false, children, className, onClick, isLoading = true }: ButtonProps) {
+function BGFadeButton({ bgFade = false, children, className, onClick, isLoading = false }: ButtonProps) {
     let buttonStyle = "rounded-md outline outline-gray-100 outline-2 hover:cursor-pointer font-semibold"
     if (bgFade) {
         buttonStyle += " text-gray-100 hover:text-navy bg-gray-100/0 hover:bg-gray-100/100 duration-300 ease-in-out"
